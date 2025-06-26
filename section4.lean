63ea7c1950133c154c0e0383b2f558d3ea6ae035
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-06-26 22:30:03 +0900

    wip: simple identities

--- section4.lean
+++ section4.lean
@@ -73,3 +73,19 @@ namespace congrExample
   example : f a = g a := congrFun h₂ a
   example : f a = g b := congr h₂ h₁
 end congrExample
+
+namespace SimpleIdentity
+  variable (a b c : Nat)
+
+  example : a + 0 = a := Nat.add_zero a
+  example : 0 + a = a := Nat.zero_add a
+  example : a * 1 = a := Nat.mul_one a
+  example : 1 * a = a := Nat.one_mul a
+  example : a + b = b + a := Nat.add_comm a b
+  example : a + b + c = a + (b + c) := Nat.add_assoc a b c
+  example : a * b = b  * a := Nat.mul_comm a b
+  example : a * (b + c) = a * b + a * c := Nat.mul_add a b c
+  example : a * (b + c) = a * b + a * c := Nat.left_distrib a b c
+  example : (a + b) * c = a * c + b * c := Nat.add_mul a b c
+  example : (a + b) * c = a * c + b * c := Nat.right_distrib a b c
+end SimpleIdentity
