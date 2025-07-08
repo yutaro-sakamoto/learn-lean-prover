9501907c28e191a4adfb420aeddf7ccba7b4da72
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-07-08 21:25:41 +0900

    wip: wip

--- section4.lean
+++ section4.lean
@@ -174,9 +174,23 @@ def divides_trans (h₁ : divides x y) (h₂ : divides y z) : divides x z :=
   let ⟨k₂, d₂⟩ := h₂
   ⟨k₁ * k₂, by rw [Nat.mul_comm k₁ k₂, Nat.mul_assoc, d₁, d₂]⟩
 
-def devides_mul (x : Nat) (k : Nat) : divides x (k*x) :=
+def divides_mul (x : Nat) (k : Nat) : divides x (k*x) :=
   ⟨k, rfl⟩
 
 instance: Trans divides divides divides where
   trans := divides_trans
+
+example (h₁ : divides x y) (h₂ : y = z) : divides x (2 * z) :=
+  calc
+    divides x y := h₁
+    _ = z := h₂
+    divides _ (2 * z) := divides_mul ..
+
+infix:50 " | " => divides
+
+example (h₁ : divides x y) (h₂ : y = z) : divides x (2 * z) :=
+  calc
+    x | y := h₁
+    _ = z := h₂
+    _ | 2  * z := divides_mul ..
 end Equations
