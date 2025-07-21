4441e76be98c0ab4312287e4c7ca28a5873cb177
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-07-21 22:27:52 +0900

    wip: wip

--- section4.lean
+++ section4.lean
@@ -437,4 +437,11 @@ example : f 0 ≤ f 3 :=
   have : f 0 ≤ f 1 := h 0
   have : f 0 ≤ f 2 := Nat.le_trans (by assumption) (h 1)
   show f 0 ≤ f 3 from Nat.le_trans (by assumption) (h 2)
+
+example : f 0 ≥ f 1 → f 1 ≥ f 2 → f 0 = f 2 :=
+  fun _ : f 0 ≥ f 1 =>
+  fun _ : f 1 ≥ f 2 =>
+  have : f 0 ≥ f 2 := Nat.le_trans ‹ f 1 ≥ f 2 › ‹ f 0 ≥ f 1 ›
+  have : f 0 ≤ f 2 := Nat.le_trans (h 0) (h 1)
+  show f 0 = f 2 from Nat.le_antisymm this ‹ f 0 ≥ f 2 ›
 end ProofLanguage
