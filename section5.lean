4a319fbffdaf1d85d7409351eb88502ab95392d0
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-07-24 00:30:49 +0900

    wip: wip

--- section5.lean
+++ section5.lean
@@ -6,3 +6,33 @@ theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
   exact hp
 
 #print test
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro hp
+  exact ⟨hq, hp⟩
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro hp; exact ⟨hq, hp⟩
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  case left => exact hp
+  case right =>
+    apply And.intro
+    case left => exact hq
+    case right => exact hp
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  case right =>
+    apply And.intro
+    case left => exact hq
+    case right => exact hp
+  case left => exact hp
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  . exact hp
+  . apply And.intro
+    . exact hq
+    . exact hp
