f2dc10614d3538e71d64c1bcd6a8fb8106a1b4e8
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-07-24 20:00:23 +0900

    wip: a tactic `intro`

--- section5.lean
+++ section5.lean
@@ -36,3 +36,26 @@ example (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
   . apply And.intro
     . exact hq
     . exact hp
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    apply Or.elim h.right
+    . intro hq
+      apply Or.inl
+      apply And.intro
+      . exact h.left
+      . exact hq
+    . intro hr
+      apply Or.inr
+      exact ⟨h.left, hr⟩
+  . intro h
+    apply Or.elim h
+    . intro hpq
+      apply And.intro
+      exact hpq.left
+      exact Or.inl hpq.right
+    . intro hpr
+      apply And.intro
+      . exact hpr.left
+      . exact Or.inr hpr.right
