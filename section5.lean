20547c40751a331fbfcd1423c139c497eef1ee6c
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-07-26 19:04:20 +0900

    wip: wip

--- section5.lean
+++ section5.lean
@@ -179,3 +179,141 @@ example : 2 + 3 = 5 := by
 example : 2 + 3 = 5 := by
   generalize h : 3 = x
   rw [← h]
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h with
+  | inl hp => apply Or.inr; exact hp
+  | inr hq => apply Or.inl; exact hq
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h with
+  | inr hq => apply Or.inl; exact hq
+  | inl hp => apply Or.inr; exact hp
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  apply Or.inr
+  assumption
+  apply Or.inl
+  assumption
+
+example (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h
+  repeat assumption
+
+example (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h <;> assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  . apply Or.inr
+    assumption
+  . apply Or.inl
+    assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  case inr h =>
+    apply Or.inl
+    assumption
+  case inl h =>
+    apply Or.inr
+    assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  case inr h =>
+    apply Or.inl
+    assumption
+  . apply Or.inr
+    assumption
+
+example (p q : Prop) : p ∧ q → q ∧ p := by
+  intro h
+  cases h with
+  | intro hp hq => constructor; exact hq; exact hp
+
+example (p q : Prop) : p ∧ q → q ∧ p := by
+  intro h
+  cases h with
+  | intro hp hq => exact ⟨hq, hp⟩
+
+example (p q : Prop) : p ∧ q → q ∧ p := by
+  intro h
+  cases h with
+  | intro hp hq => apply And.intro; exact hq; exact hp
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    cases h with
+    | intro hp hqr =>
+      cases hqr
+      . apply Or.inl; constructor <;> assumption
+      . apply Or.inr; constructor <;> assumption
+  . intro h
+    cases h with
+    | inl hpq =>
+      cases hpq with
+        | intro hp hq => constructor; exact hp; exact Or.inl hq
+    | inr hpr =>
+      cases hpr with
+        | intro hp hr => constructor; exact hp; exact Or.inr hr
+
+example (p q : Nat → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x := by
+  intro h
+  cases h with
+  | intro x px => constructor; exact Or.inl px
+
+example (p q : Nat → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x := by
+  intro h
+  cases h with
+  | intro x px => apply Exists.intro; apply Or.inl; exact px
+
+example (p q : Nat → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x := by
+  intro h
+  cases h with
+  | intro x px => exists x; apply Or.inl; exact px
+
+example (p q : Nat → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro h
+  cases h with
+  | intro x hpq =>
+    cases hpq with
+    | intro hp hq => exists x
+
+def swap_pair : α × β → β × α := by
+  intro p
+  cases p
+  constructor <;> assumption
+
+def swap_sum : Sum α β → Sum β α := by
+  intro p
+  cases p
+  . apply Sum.inr; assumption
+  . apply Sum.inl; assumption
+
+theorem swap_and : a ∧ b → b ∧ a := by
+  intro p
+  cases p
+  constructor <;> assumption
+
+theorem swap_or : a ∨ b → b ∨ a := by
+  intro p
+  cases p
+  . apply Or.inr; assumption
+  . apply Or.inl; assumption
+
+open Nat
+example (P : Nat → Prop) (h₀ : P 0) (h₁ : ∀ n, P (succ n)) (m : Nat) : P m := by
+  cases m with
+  | zero => exact h₀
+  | succ m' => exact h₁ m'
