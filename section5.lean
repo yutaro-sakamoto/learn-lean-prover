feab9738fe2b0b00535c2ba4c6eb6f253da74dca
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-08-08 22:06:11 +0900

    wip: wip

--- section5.lean
+++ section5.lean
@@ -523,3 +523,115 @@ def Tuple (α : Type) (n : Nat) :=
 example (n : Nat) (h : n = 0) (t : Tuple α n) : Tuple α 0 := by
   rw [h] at t
   exact t
+
+example (x y z : Nat) : (x + 0) * (0 + y * 1 + z * 0) = x * y := by
+  simp
+
+example (x y z : Nat) (p : Nat → Prop) (h : p (x * y))
+  : p ((x + 0) * (0 + y * 1 + z * 0)) := by
+  simp; assumption
+
+open List
+
+example (xs : List Nat)
+  : reverse (xs ++ [1, 2, 3]) = [3, 2, 1] ++ reverse xs := by
+  simp
+
+example (xs ys : List α)
+  : length (reverse (xs ++ ys)) = length xs + length ys := by
+  simp [Nat.add_comm]
+
+open List
+
+example (x y z : Nat) (p : Nat → Prop)
+  (h : p ((x + 0) * (0 + y * 1 + z * 0))) : p (x * y) := by
+  simp at h; assumption
+
+section local_simp
+attribute [local simp] Nat.mul_comm Nat.mul_assoc Nat.mul_left_comm
+attribute [local simp] Nat.add_assoc Nat.add_comm Nat.add_left_comm
+
+example (w x y z : Nat) (p : Nat → Prop)
+  (h : p (x * y + z * w * x)) : p (x * w * z + y * x) := by
+  simp at *; assumption
+
+example (x y z : Nat) (p : Nat → Prop)
+    (h₁ : p (1 * x + y)) (h₂ : p (x * z * 1))
+    : p (y + 0 + x) ∧ p (z * x) := by
+  simp at * <;> constructor <;> assumption
+
+example (w x y z : Nat)
+    : x * y + z * w * x = x * w * z + y * x := by
+  simp
+
+example (w x y z : Nat) (p : Nat → Prop)
+    (h : p (x * y + z * w * x)) : p (x * w * z + y * x) := by
+  simp
+  simp at h
+  assumption
+end local_simp
+
+def f (m n : Nat) : Nat :=
+  m + n + m
+
+example {m n : Nat} (h : n = 1) (h' : 0 = m) : (f m n) = n := by
+  simp [h, ←h', f]
+
+example (f : Nat →  Nat) (k : Nat) (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+  simp [h₁, h₂]
+
+example (f : Nat →  Nat) (k : Nat) (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+  rw [h₂, h₁]
+
+example (f : Nat → Nat) (k : Nat) (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+  simp [*]
+
+example (u w x y z : Nat) (h₁ : x = y + z) (h₂ : w = u + x)
+    : w = z + y + u := by
+  simp [*, Nat.add_assoc, Nat.add_comm]
+
+example (p q : Prop) (hp : p) : p ∧ q ↔ q := by
+  simp [*]
+
+example (p q : Prop) (hp : p) : p ∨ q := by
+  simp [*]
+
+example (p q r : Prop) (hp : p) (hq : q) : p ∧ (q ∨ r) := by
+  simp [*]
+
+example (u w x x' y y' z : Nat) (p : Nat → Prop)
+    (h₁ : x + 0 = x') (h₂ : y + 0 = y')
+    : x + y + 0 = x' + y' := by
+  simp at *
+  simp [*]
+
+def mk_symm (xs : List α) :=
+  xs ++ xs.reverse
+
+@[simp] theorem reverse_mk_symm (xs : List α)
+    : (mk_symm xs).reverse = mk_symm xs := by
+  simp [mk_symm]
+
+example (xs ys : List Nat)
+    : (xs ++ mk_symm ys).reverse = mk_symm ys ++ xs.reverse := by
+    simp [reverse_mk_symm]
+
+example (xs ys : List Nat) (p : List Nat → Prop)
+    (h : p (xs ++ mk_symm ys).reverse)
+    : p (mk_symm ys ++ xs.reverse) := by
+  simp [reverse_mk_symm] at h; assumption
+
+example (xs ys : List Nat)
+    : (xs ++ mk_symm ys).reverse = mk_symm ys ++ xs.reverse := by
+  simp
+
+example (xs ys : List Nat) (p : List Nat → Prop)
+    (h : p (xs ++ mk_symm ys).reverse)
+    : p (mk_symm ys ++ xs.reverse) := by
+  simp at h; assumption
+
+example : if x = 0 then y + x = y else x ≠ 0 := by
+  simp (config := { contextual := true})
+
+example : ∀ (x : Nat) (h : x = 0), y + x = y := by
+  simp (config := { contextual := true})
